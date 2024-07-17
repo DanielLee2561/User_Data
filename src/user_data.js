05241a6484d475d659db2aa1e8bcfab5382c71a3
DanielLee2561 <daniel.j.lee.2561@gmail.com>
2024-07-17 10:41:23 -0700

    Update Q9/10

--- src/user_data.js
+++ src/user_data.js
@@ -10,6 +10,7 @@ function Attempt(inProgress) {
   this.inProgress       = inProgress;
   this.startTime        = null;
   this.endTime          = null;
+  this.duration         = null;
   this.generatedCode    = null;
 	this.failingTestCases = null;
   this.testCorrect      = null;
@@ -19,6 +20,7 @@ function Attempt(inProgress) {
   if (!inProgress) {
     this.startTime        = new Date("2024-07-01T00:00:00");
     this.endTime          = new Date("2024-07-01T00:10:00");
+    this.duration         = (this.endTime - this.startTime) / 1000;
     this.generatedCode    = "LLM"
     this.failingTestCases = "testsFailed"
     this.testCorrect      = 1;
@@ -51,14 +53,16 @@ function User(username, password, type)
   // Questions
   let questions = [];
 
-  QA_1 = new Question_Attempts(1, [false, false, true]);  questions.push(QA_1);
-  QA_2 = new Question_Attempts(2, [false]);               questions.push(QA_2);
-  QA_3 = new Question_Attempts(3, [true]);                questions.push(QA_3);
-  QA_4 = new Question_Attempts(4, []);                    questions.push(QA_4);
-  QA_5 = new Question_Attempts(5, []);                    questions.push(QA_5);
-  QA_6 = new Question_Attempts(6, []);                    questions.push(QA_6);
-  QA_7 = new Question_Attempts(7, []);                    questions.push(QA_7);
-  QA_8 = new Question_Attempts(8, []);                    questions.push(QA_8);
+  QA_1 = new Question_Attempts(1, [false, false, false, true]); questions.push(QA_1);
+  QA_2 = new Question_Attempts(2, [false]);                     questions.push(QA_2);
+  QA_3 = new Question_Attempts(3, [false]);                     questions.push(QA_3);
+  QA_4 = new Question_Attempts(4, [false]);                     questions.push(QA_4);
+  QA_5 = new Question_Attempts(5, [false]);                     questions.push(QA_5);
+  QA_6 = new Question_Attempts(6, [false]);                     questions.push(QA_6);
+  QA_7 = new Question_Attempts(7, [false]);                     questions.push(QA_7);
+  QA_8 = new Question_Attempts(8, [false]);                     questions.push(QA_8);
+  QA_9 = new Question_Attempts(9, [true]);                      questions.push(QA_9);
+  QA_10 = new Question_Attempts(10, []);                        questions.push(QA_10);
 
   this.questions = questions;
 }
